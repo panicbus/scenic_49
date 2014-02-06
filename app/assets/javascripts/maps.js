a3fb8ec1a582cab085445a7ddcde70320810b5ad
panicbus <elementalair@gmail.com>
2014-02-05 17:48:33 -0800

    fixed jQuery issue, added about modal

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,13 +1,20 @@
-$(function(){
-	initialize_map();
-});
+// $(function(){
+// 	initialize_map();
+// });
 
 // var initialize_map(){
 
+// 	var layer = new L.StamenTileLayer("watercolor");
+
+// 	 map = new L.mapbox.map('map');
+
+// 	  // this adds the layer created above to the map //
+// 	  map.addLayer(layer);
+// 	  map.setView([40, -74.50], 9);
+// 	  map.addControl(L.mapbox.geocoderControl('panicbus.h4on0f5b', {
+// 	        keepOpen: true
+// 	    }));
+
 // };
-
-
-var map = L.mapbox.map('	map', 'panicbus.h4on0f5b')
-  .setView([40, -74.50], 9);
 
 
