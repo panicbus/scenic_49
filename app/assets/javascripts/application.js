e5e2fa23f28fdf589dff2068ffbee488697f194a
panicbus <elementalair@gmail.com>
2014-02-07 19:41:51 -0800

    added innitial marker events

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,39 +15,3 @@
 //= require_tree .
 
 
-
-
-// var map = L.mapbox.map('map', 'panicbus.h4on0f5b')
-//     .setView([0, 0], 1);
-
-// // Generate a GeoJSON line: you can load GeoJSON via AJAX, or
-// // generate it some other way
-// var geojson = { type: 'LineString', coordinates: [] },
-//     start = [10, 20],
-//     momentum = [3, 3];
-// for (var i = 0; i < 300; i++) {
-//     start[0] += momentum[0];
-//     start[1] += momentum[1];
-//     if (start[1] > 60 || start[1] < -60) momentum[1] *= -1;
-//     if (start[0] > 170 || start[0] < -170) momentum[0] *= -1;
-//     geojson.coordinates.push(start.slice());
-// }
-
-// var geojsonLayer = L.geoJson(geojson).addTo(map),
-//     marker = L.marker([0, 0], {
-//         icon: L.mapbox.marker.icon()
-//     }).addTo(map),
-//     j = 0;
-
-// tick();
-// function tick() {
-//     // set the marker to be at the same point as one of the segments
-//     // of the line
-//     marker.setLatLng(L.latLng(
-//         geojson.coordinates[j][1],
-//         geojson.coordinates[j][0]));
-
-//     // move to the next point in the line or loop to the first point if
-//     // j runs off the end of the array
-//     if (++j < geojson.coordinates.length) setTimeout(tick, 100);
-// }
--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,20 +1,91 @@
-// $(function(){
-// 	initialize_map();
-// });
+$(function(){
+	initialize_map();
+});
 
-// var initialize_map(){
+function initialize_map(){
 
 // 	var layer = new L.StamenTileLayer("watercolor");
+// 	var map = new L.mapbox.map('map');
 
-// 	 map = new L.mapbox.map('map');
-
-// 	  // this adds the layer created above to the map //
+// // 	  // this adds the layer created above to the map //
 // 	  map.addLayer(layer);
 // 	  map.setView([40, -74.50], 9);
 // 	  map.addControl(L.mapbox.geocoderControl('panicbus.h4on0f5b', {
 // 	        keepOpen: true
 // 	    }));
 
-// };
+
+	var	map = new L.mapbox.map('map', 'panicbus.h4on0f5b').setView([37.775,-122.419], 13);
+
+	// the json data for marker locations
+	var geoJson = [
+	    {
+	        type: 'Feature',
+	        geometry: {
+	            type: 'Point',
+	            coordinates: [-122.453, 37.771]
+	        },
+	        properties: {
+	            title: 'Marker one',
+	            description: '<strong>Wow</strong>, this tooltip is breaking all the rules.',
+	            'marker-color': '#548cba'
+	        }
+	    },
+	    {
+	        type: 'Feature',
+	        geometry: {
+	            type: 'Point',
+	            coordinates: [-122.413, 37.774]
+	        },
+	        properties: {
+	            title: 'Marker two',
+	            description: 'Another marker, including <a href="http://mapbox.com">a link</a>.',
+	            'marker-color': '#548cba'
+	        }
+	    }
+	];
+	// adds the json to the map layer
+	map.markerLayer.setGeoJSON(geoJson);
+
+	// this is the marker click listener 
+	map.markerLayer.on('click',function(e) {
+    var feature = e.layer.feature;
+    var info = '<h2>' + feature.properties.title + '</h2>' +
+               '<p>' + feature.properties.description + '</p>';
+
+    document.getElementById('site-1').innerHTML = info;
+});
+
+// Clear the tooltip when map is clicked
+map.on('click',function(e){
+    document.getElementById('site-1').innerHTML = '';
+});
+
+
+
+
+	map.markerLayer.on('layeradd', function(e) { 
+    var marker = e.layer, 
+        feature = marker.feature; 
+
+		var popupContent = '<p>This is the content</p>' +
+											 '<form>' +
+											 'First name: <input type="text" name="firstname"><br>' +
+											 'Last name: <input type="text" name="lastname">' +
+											 '</form>'; 
+
+		marker.bindPopup(popupContent,{ 
+			closeButton: true, 
+			minWidth: 320 
+		}); 
+
+	});
+
+// map.markerLayer.setGeoJSON(geoJson);
+
+	myMarkers.loadURL('data.json');
+
+
+};
 
 
