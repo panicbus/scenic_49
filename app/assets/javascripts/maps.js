6421f1f9687d743e63720fdb6f7285096b8e00c6
panicbus <elementalair@gmail.com>
2014-02-08 16:05:12 -0800

    proper popup binding for click events

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -17,80 +17,25 @@ function initialize_map(){
 
 	var	map = new L.mapbox.map('map', 'panicbus.h4on0f5b').setView([37.775,-122.419], 13);
 
-	// the json data for marker locations
-	var geoJson_point = [
-	    {
-	        type: 'Feature',
-	        geometry: {
-	            type: 'Point',
-	            coordinates: [-122.453, 37.771]
-	        },
-	        properties: {
-	            title: 'Marker one',
-	            description: 'The content of marker one.',
-	            'marker-color': '#548cba'
-	        }
-	    },
-	    {
-	        type: 'Feature',
-	        geometry: {
-	            type: 'Point',
-	            coordinates: [  -122.46032953262329, 37.76966311138133]
-	        },
-	        properties: {
-	            title: 'Marker two',
-	            description: 'The content of marker two, with <a href="http://mapbox.com">a link</a>.',
-	            'marker-color': '#548cba'
-	        }
-	    },
-	    {
-			  type: "Feature",
-			  properties: {},
-			  geometry: {
-			    type: "LineString",
-			    coordinates: [
-			      [
-			        -122.46695995330812,
-			        37.76805171823878
-			      ],
-			      [
-			        -122.46455669403076,
-			        37.76966311138133
-			      ],
-			      [
-			        -122.46161699295044,
-			        37.770358543890254
-			      ],
-			      [
-			        -122.46024370193481,
-			        37.770358543890254
-			      ],
-			      [
-			        -122.45947122573853,
-			        37.77079954648267
-			      ],
-			      [
-			        -122.45923519134521,
-			        37.77129143088672
-			      ],
-			      [
-			        -122.4558663368225,
-			        37.77088435437199
-			      ],
-			      [
-			        -122.45535135269164,
-			        37.77078258489315
-			      ],
-			      [
-			        -122.45537281036377,
-			        37.770443352285376
-			      ]
-			    ]
-			  }
-			}
-	];
-	// adds the json to the map layer
-	map.featureLayer.setGeoJSON(geoJson_point);
+
+
+		// adds the json to the map layer from route
+	map.featureLayer.setGeoJSON(geoJson_features);
+
+	map.featureLayer.eachLayer(function(layer){
+
+		var popupContent = '<p>This is the content</p>' +
+											 '<form>' +
+											 'First name: <input type="text" name="firstname"><br>' +
+											 'Last name: <input type="text" name="lastname">' +
+											 '</form>'; 
+
+		layer.bindPopup(popupContent,{ 
+			closeButton: true, 
+			minWidth: 320 
+		}); 
+
+	})
 
 
 	// this is the marker click listener 
@@ -100,37 +45,12 @@ function initialize_map(){
                '<p>' + feature.properties.description + '</p>';
 
     document.getElementById('site-1').innerHTML = info;
-});
-
-// Clear the tooltip when map is clicked
-map.on('click',function(e){
-    document.getElementById('site-1').innerHTML = '';
-});
-
-
-
-
-	map.featureLayer.on('layeradd', function(e) { 
-    var marker = e.layer, 
-        feature = marker.feature; 
-
-		var popupContent = '<p>This is the content</p>' +
-											 '<form>' +
-											 'First name: <input type="text" name="firstname"><br>' +
-											 'Last name: <input type="text" name="lastname">' +
-											 '</form>'; 
-
-		marker.bindPopup(popupContent,{ 
-			closeButton: true, 
-			minWidth: 320 
-		}); 
-
 	});
 
-// map.featureLayer.setGeoJSON(geoJson);
-
-	// myMarkers.loadURL('data.json');
-
+	// Clear the tooltip when map is clicked
+	map.on('click',function(e){
+	    document.getElementById('site-1').innerHTML = '';
+	});
 
 };
 
