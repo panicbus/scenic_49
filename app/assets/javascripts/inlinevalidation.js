e4c52c861c92af93aa8aaf062b1967c1990fbf85
panicbus <elementalair@gmail.com>
2014-02-18 15:32:18 -0800

    removing unused files

--- app/assets/javascripts/inlinevalidation.js
+++
@@ -1,8 +0,0 @@
-// for error message when name field is not filled out 
-
-function inlineValidation(){
-	// alert('first!')
-	$('#name').blur(function(event){
-		alert('working!!');
-	})
-}
--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,6 +1,5 @@
 $(function(){
 	initialize_map();
-	inlineValidation();
 });
 
 
