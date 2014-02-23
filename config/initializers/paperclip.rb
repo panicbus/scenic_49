64a2e96b74e0ab3fef645533326ca889d57e90a3
Chris Weilemann <chris.weilemann@hs2solutions.com>
2014-02-23 14:15:41 -0600

    initial commit for fixing paperclip via AJAX

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,7 @@
-Paperclip::Attachment.default_options[:url] = 'scenic49.s3-website-us-west-1.amazonaws.com'
-Paperclip::Attachment.default_options[:path] = '/:filename'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+Paperclip::Attachment.default_options[:storage] = :s3
+Paperclip::Attachment.default_options[:s3_protocol] = 'http'
+Paperclip::Attachment.default_options[:s3_credentials] =
+  { :bucket => ENV['AWS_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY'],
+    :secret_access_key => ENV['AWS_SECRET_KEY'] }
