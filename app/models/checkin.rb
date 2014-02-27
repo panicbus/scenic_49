116e38265712f3a7cbe4233ff4a709e4bafab04f
panicbus <elementalair@gmail.com>
2014-02-27 14:14:11 -0800

    fixed photo upload error caused by git pull

--- app/models/checkin.rb
+++ app/models/checkin.rb
@@ -14,12 +14,8 @@ class Checkin < ActiveRecord::Base
                       thumb: '100x100>',
                       square: '200x200#',
                       medium: '300x300>'
-                    },
-                    :bucket => 'scenic49',
-                    :storage => :s3,
-                    :s3_host_name => 's3-website-us-west-1.amazonaws.com/',
-                    :s3_credentials => S3_CREDENTIALS
-
+                    }
+                    
   # Validate the attached image is image/jpg, image/png, etc
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 
