ed2a38c12ba03b8664773ef3c8f8dd0f4f879bd2
panicbus <elementalair@gmail.com>
2014-02-27 23:25:49 -0800

    upgraded sidebar scolling

--- app/models/checkin.rb
+++ app/models/checkin.rb
@@ -13,7 +13,7 @@ class Checkin < ActiveRecord::Base
                     styles: {
                       thumb: '100x100>',
                       square: '200x200#',
-                      medium: '300x300>'
+                      medium: '325x300>'
                     }
                     
   # Validate the attached image is image/jpg, image/png, etc
