06b157dd3f01541be2771acfffdc085f8acf8a29
panicbus <elementalair@gmail.com>
2014-03-01 16:48:19 -0800

    close collapse nav on station click

--- app/assets/javascripts/dropdownNav.js
+++ app/assets/javascripts/dropdownNav.js
@@ -2,38 +2,56 @@ $(document).ready(function(){
 
 	$('.drop1').on('click',function(e) {
 		$.get("/checkins/1"); 
+		$('.content_div').addClass('enbiggen');
+		$('.navbar-toggle').click();
 	});
 
 	$('.drop2').on('click',function(e) {
 		$.get("/checkins/2");
+		$('.content_div').addClass('enbiggen');
+		$('.navbar-toggle').click();
 	});
 
 	$('.drop3').on('click',function(e) {
 		$.get("/checkins/3");
+		$('.content_div').addClass('enbiggen');
+		$('.navbar-toggle').click();
 	});
 
 	$('.drop4').on('click',function(e) {
 		$.get("/checkins/4");
+		$('.content_div').addClass('enbiggen');
+		$('.navbar-toggle').click();
 	});
 
 	$('.drop5').on('click',function(e) {
 		$.get("/checkins/5");
+		$('.content_div').addClass('enbiggen');
+		$('.navbar-toggle').click();
 	});
 
 	$('.drop6').on('click',function(e) {
 		$.get("/checkins/6");
+		$('.content_div').addClass('enbiggen');
+		$('.navbar-toggle').click();
 	});
 
 	$('.drop7').on('click',function(e) {
 		$.get("/checkins/7");
+		$('.content_div').addClass('enbiggen');
+		$('.navbar-toggle').click();
 	});
 
 	$('.drop8').on('click',function(e) {
 		$.get("/checkins/8");
+		$('.content_div').addClass('enbiggen');
+		$('.navbar-toggle').click();
 	});
 
 	$('.drop9').on('click',function(e) {
 		$.get("/checkins/9");
+		$('.content_div').addClass('enbiggen');
+		$('.navbar-toggle').click();
 	});
 
 })
