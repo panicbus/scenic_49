a7ae0d364571a50c5b220bcb2c9ab73a983745ab
panicbus <elementalair@gmail.com>
2014-03-11 12:29:22 -0700

    removing stamen overlay

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -18,7 +18,7 @@ function initialize_map(){
 		var	map = new L.mapbox.map('map', 'panicbus.h4on0f5b').setView([37.735, -122.443], 12); //set map center for mobile size
 	};
 
-	var stamenLayer = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.png', {attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.'}).addTo(map);
+	// var stamenLayer = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.png', {attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.'}).addTo(map);
 
 
 	// adds the GeoJson to the map layer from route.js
